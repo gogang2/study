5befb9c91cbd082589273ed8c9d90b7614b1670c
gogang2 <g2m0611@naver.com>
2021-10-05 22:08:09 +0900

    first commit

---
+++ src/backjun/type.java
@@ -0,0 +1,18 @@
+package backjun;
+
+public class type {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		String min = "han2";
+		
+		System.out.println(i);
+		System.out.println(min);
+		
+		System.out.println("Hello World");
+		
+		System.out.println("æ»≥Á«œººø‰");
+	}
+
+}
